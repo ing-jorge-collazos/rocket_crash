7c4d209e2c5c8bec758f47d336993c35e2377d11
ingjorgecollazosn <ingjorgecollazosn@outlook.com>
2024-03-19 20:38:43 -0500

    feat: add basic movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    
+    Rigidbody rb;
+    [SerializeField] float rocketVelocity = 100f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
    
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+     void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * rocketVelocity * Time.deltaTime); //Es lo mismo que (0,1,0) vector3.up abrevia
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotate Left");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotate Right");
+        }
     }
 }
